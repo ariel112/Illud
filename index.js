5aca9aeb4d90eecb15e3e08b767928c0998f2c53
ariel112 <selvinmorazan@gmail.com>
2018-12-04 21:16:38 -0600

    redes sociales

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 var express = require("express");
 var app= express();
 
+/*base de datos mysql*/
+var mysql = require('mysql');
+
+app.use(express.static("public"));
+var credenciales = {
+    user: "root",
+    password:"",
+    database:"illud_db",
+    host:"localhost",
+    port:"3306"
+};
+
+
 //exponer una carpeta como publica, unicamente para archivos estaticos: html, img, css
 app.use(express.static("public"));
 
